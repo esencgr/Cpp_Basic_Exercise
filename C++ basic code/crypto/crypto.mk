541d2d22bf6c01fe3a74eb575dd72d9be09e248f
Cagri Esen <esencgr@gmail.com>
2018-10-06 23:36:12 +0300

    add file

---
+++ C++ basic code/crypto/crypto.mk
@@ -0,0 +1,112 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=crypto
+ConfigurationName      :=Debug
+WorkspacePath          :="D:/C Codes/Cpp_general_example"
+ProjectPath            :="D:/C Codes/Cpp_general_example/crypto"
+IntermediateDirectory  :=./Debug
+OutDir                 := $(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=çağrı
+Date                   :=04/10/2018
+CodeLitePath           :="C:/Program Files/CodeLite"
+LinkerName             :=C:/TDM-GCC-64/bin/g++.exe
+SharedObjectLinkerName :=C:/TDM-GCC-64/bin/g++.exe -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.i
+DebugSwitch            :=-g 
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputFile             :=$(IntermediateDirectory)/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E
+ObjectsFileList        :="crypto.txt"
+PCHCompileFlags        :=
+MakeDirCommand         :=makedir
+RcCmpOptions           := 
+RcCompilerName         :=C:/TDM-GCC-64/bin/windres.exe
+LinkOptions            :=  
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overriden using an environment variables
+##
+AR       := C:/TDM-GCC-64/bin/ar.exe rcu
+CXX      := C:/TDM-GCC-64/bin/g++.exe
+CC       := C:/TDM-GCC-64/bin/gcc.exe
+CXXFLAGS :=  -g -O0 -Wall $(Preprocessors)
+CFLAGS   :=  -g -O0 -Wall $(Preprocessors)
+ASFLAGS  := 
+AS       := C:/TDM-GCC-64/bin/as.exe
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=C:\Program Files\CodeLite
+Objects0=$(IntermediateDirectory)/crypto.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) $(@D)
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "./Debug"
+
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "./Debug"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/crypto.cpp$(ObjectSuffix): crypto.cpp $(IntermediateDirectory)/crypto.cpp$(DependSuffix)
+	$(CXX) $(IncludePCH) $(SourceSwitch) "D:/C Codes/Cpp_general_example/crypto/crypto.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/crypto.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/crypto.cpp$(DependSuffix): crypto.cpp
+	@$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) -MG -MP -MT$(IntermediateDirectory)/crypto.cpp$(ObjectSuffix) -MF$(IntermediateDirectory)/crypto.cpp$(DependSuffix) -MM crypto.cpp
+
+$(IntermediateDirectory)/crypto.cpp$(PreprocessSuffix): crypto.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/crypto.cpp$(PreprocessSuffix) crypto.cpp
+
+
+-include $(IntermediateDirectory)/*$(DependSuffix)
+##
+## Clean
+##
+clean:
+	$(RM) -r ./Debug/
+
+
---
+++ C++ basic code/number_reading/Debug/number_reading.cpp.o.d
@@ -0,0 +1,1 @@
+Debug/number_reading.cpp.o: number_reading.cpp
