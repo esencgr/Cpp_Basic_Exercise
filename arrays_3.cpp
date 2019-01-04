79a9bc84283ffa6d76df349d046d3e2100e1fd09
Cagri Esen <esencgr@gmail.com>
2019-01-04 16:16:15 +0300

    modified

--- arrays_3.cpp
+++ arrays_3.cpp
@@ -5,19 +5,14 @@ int main()
 {
 
 int c[3][3];
-	
-    for (int i = 0; i < 3; i++){
-	
-	 for (int j = 0; j < 3; j++)  
-	     c[i][j] = i + j;			
-    }
-    
-    for (int i = 0; i < 3; i++){
-	
-	 for (int j = 0; j < 3; j++)  
-             cout<< c[i][j]<< '  ';	
-	     cout<< endl; 
-    }
 
+  for (int i = 0; i < 3; i++)
+	    for (int j = 0; j < 3; j++)
+	         c[i][j] = i + j;
+
+  for (int i = 0; i < 3; i++){
+      for (int j = 0; j < 3; j++)
+           cout<< c[i][j]<< '  ';
+  cout<< endl;
+    }
 }
-
