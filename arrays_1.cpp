9fb30f9ecf41e251fb65d2329c928b93e75e280d
Cagri Esen <esencgr@gmail.com>
2019-01-04 14:15:11 +0000

    add file

--- arrays_1.cpp
+++ arrays_1.cpp
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-int main () 
+int main ()
 {
 	int a[3] = {5, 6, 2};
 	cout<< a[1] <<endl;
 	cout<< a[2] + a[0] << endl;
 	a[2] = 8;
 	cout<< "a={";
-	
+
 	    for (int i = 0; i <= 2; i++)
 	        cout<< a[i] << ' ';
 	        cout<< "}" << endl;
-   
+
 	int b[4];
         b[0] = 10;
         b[1] = 20;
         cin>> b[2];
         cin>> b[3];
-    
+
 	    cout<< "b={";
-	
+
 	    for (int i = 0; i <= 3; i++)
 	        cout<< b[i] << ' ';
 	        cout<< "}" ;
-    
+
 }
