5325be79c104c4cf28672035382a7d78d50cca12
Cagri Esen <esencgr@gmail.com>
2019-01-04 16:39:51 +0000

    add file

---
+++ new_code.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "donguler" << endl;
+
+    int a = 1; // (initialization)- tipi ve ismi
+
+    while (a <= 10){
+        cout << "merhaba" << a <<endl;
+        a++;
+        //a=100;
+    }
+
+    cout << a << endl;
+    return 0;
+}
