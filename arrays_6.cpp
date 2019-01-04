519ad595be12d9c9e2592593e26ce4b42a0b42e5
Cagri Esen <esencgr@gmail.com>
2019-01-04 16:25:01 +0000

    add file

--- arrays_6.cpp
+++ arrays_6.cpp
@@ -11,21 +11,17 @@ int main()
     int a[n];
     int toplam = 0;
         for (int i = 0; i < n; i++){
-	    cin>> a[i] ;
-	    toplam = toplam + a[i];
+	         cin>> a[i] ;
+	         toplam = toplam + a[i];
         }
-     int eb = a[0];
-     int ek = a[0];
-     for (int i = 0; i < n; i++){
-			
-	     if (eb < a[i])
-                 eb = a[i];	
-		           
-	     if (ek > a[i])
-		 ek = a[i];   
-     } 
-	
+    int eb = a[0];
+    int ek = a[0];
+        for (int i = 0; i < n; i++){
+	           if (eb < a[i])
+                eb = a[i];
+		         if (ek > a[i])
+		            ek = a[i];
+        }
     cout<< "maks:" << toplam-ek << endl ;
     cout<< "min:" << toplam-eb << endl;
-    
 }
