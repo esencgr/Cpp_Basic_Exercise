1ed4bf3f7b3e93d0d491249db5b6607b8339aa02
Cagri Esen <esencgr@gmail.com>
2019-01-04 16:08:05 +0300

    modified

--- arrays_1.cpp
+++ arrays_1.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 int main ()
@@ -24,34 +23,3 @@ int main ()
 	        cout<< b[i] << ' ';
 	    cout<< "}";
 }
-=======
-#include<iostream>
-
-using namespace std;
-
-int main ()
-{
-	int a[3] = {5, 6, 2};
-	cout<< a[1] <<endl;
-	cout<< a[2] + a[0] << endl;
-	a[2] = 8;
-	cout<< "a={";
-
-	    for (int i = 0; i <= 2; i++)
-	        cout<< a[i] << ' ';
-	        cout<< "}" << endl;
-
-	int b[4];
-        b[0] = 10;
-        b[1] = 20;
-        cin>> b[2];
-        cin>> b[3];
-
-	    cout<< "b={";
-
-	    for (int i = 0; i <= 3; i++)
-	        cout<< b[i] << ' ';
-	        cout<< "}" ;
-
-}
->>>>>>> 4bf58345249754afa1415298b3478bed3cb3b2c5
