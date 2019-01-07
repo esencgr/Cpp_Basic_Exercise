7e6b3a4d66304cbbd16b726df89fc8168ae2230e
Cagri Esen <esencgr@gmail.com>
2019-01-07 21:49:44 +0300

    delete

--- avarage_die_count.cpp
+++ avarage_die_count.cpp
@@ -5,101 +5,101 @@
 #include<math.h>
 
 using namespace std;
- 
+
 float hesap(){
-	
-int eleman; 
-cout << "Dizi kaç elemanlý olacak?: "; 
+
+int eleman;
+cout << "Dizi kaï¿½ elemanlï¿½ olacak?: ";
 cin >> eleman; 
-      
- 
+
+
       int dizi[eleman], i;
-      float aritmetik,statik,toplam=0.0; 
+      float aritmetik,statik,toplam=0.0;
       cout << "girilen dizi: ";
 
  	     for(i = 0; i < eleman; i++)
   		cin>> dizi[i];
-  		      
- 	     
-            for(i = 0 ; i < eleman; i++)  		        
+
+
+            for(i = 0 ; i < eleman; i++)
 		toplam += dizi[i];
-  		  
-	       aritmetik = toplam/eleman; 
- 		
-cout <<"Girilen toplam eleman deðeri: " << toplam<<endl; // Bilgi amaçlý.
+
+	       aritmetik = toplam/eleman;
+
+cout <<"Girilen toplam eleman deï¿½eri: " << toplam<<endl; // Bilgi amaï¿½lï¿½.
 toplam = 0.0;
- 	    for( i = 0; i < eleman; i++) 
- 	         
- 	       toplam += pow(dizi[i] - aritmetik , 2.0); // formülün uyarlamasý.
-	           
-	
-	            statik = sqrt (toplam / (eleman-1)); // Sapmayý hesapladým ve statik deðiþkenine atadým.
-		    cout << "Aritmetik ortalama :" << aritmetik << endl; // Aritmetik sonuç.
-		    cout << "Standart sapma degeri:" << statik;		  // Statik sonuç.
-		        
-} 
-    
+ 	    for( i = 0; i < eleman; i++)
+
+ 	       toplam += pow(dizi[i] - aritmetik , 2.0); // formï¿½lï¿½n uyarlamasï¿½.
+
+
+	            statik = sqrt (toplam / (eleman-1)); // Sapmayï¿½ hesapladï¿½m ve statik deï¿½iï¿½kenine atadï¿½m.
+		    cout << "Aritmetik ortalama :" << aritmetik << endl; // Aritmetik sonuï¿½.
+		    cout << "Standart sapma degeri:" << statik;		  // Statik sonuï¿½.
+
+}
+
 	void zar()
 {
 double sayi,i,k,tut,devam;
 double olasilik,hedef;
 int c;
-        while(1) // Deney sürekli tekrar edebilsin {
+        while(1) // Deney sï¿½rekli tekrar edebilsin {
             k = 0;
-     
-            cout<< "Zar kaç kere atýlacak?: ";
+
+            cout<< "Zar kaï¿½ kere atï¿½lacak?: ";
             cin>> i;
-     
+
 	    tut = i;
 	    basla:
-     
-	    cout<< "Hangi durum hesaplansýn?:  [1,2,3,4,5,6]: ";
+
+	    cout<< "Hangi durum hesaplansï¿½n?:  [1,2,3,4,5,6]: ";
             cin>> hedef;
-     
-                if(hedef < 1 || hedef > 6) // Seçimi denetledim.{
-	            cout <<"Geçersiz seçim yaptýnýz.";
+
+                if(hedef < 1 || hedef > 6) // Seï¿½imi denetledim.{
+	            cout <<"Geï¿½ersiz seï¿½im yaptï¿½nï¿½z.";
               	    goto basla;
-                } 
-             srand( time (NULL)); // Rastgele sayý çekmek için.
-     	
-		while(i != 0) // Olur ya hani sýfýr denk gelirse diye.
-	 	{ 
+                }
+             srand( time (NULL)); // Rastgele sayï¿½ ï¿½ekmek iï¿½in.
+
+		while(i != 0) // Olur ya hani sï¿½fï¿½r denk gelirse diye.
+	 	{
      	            sayi=rand()%6+1;
      	            cout<<"Gelen zar: "<<sayi<<endl;
-     	
+
 		        if(sayi==hedef)
 		           {
-			        k += 1; // Atýlan zarlar.
-		           } 
-     		        i--; // Atýþý sýnýrladým. Girdiðim sayýdan azalacak.
-     	        }	
+			        k += 1; // Atï¿½lan zarlar.
+		           }
+     		        i--; // Atï¿½ï¿½ï¿½ sï¿½nï¿½rladï¿½m. Girdiï¿½im sayï¿½dan azalacak.
+     	        }
         cout<<endl;
-        cout<<"Seçtiðiniz zarýn gelme olasýlýðý: "<<(k/tut)<<endl; // Sonuç.
-     
-        cout<<"Deney tekrarý için: 1 - Deney sonu için: 2 ---> "; // Tekrar teklifi.
+        cout<<"Seï¿½tiï¿½iniz zarï¿½n gelme olasï¿½lï¿½ï¿½ï¿½: "<<(k/tut)<<endl; // Sonuï¿½.
+
+        cout<<"Deney tekrarï¿½ iï¿½in: 1 - Deney sonu iï¿½in: 2 ---> "; // Tekrar teklifi.
         cin>>devam;
                if(devam==1)
-	             
+
 	 	  continue; // Devam etsin.
-	             
- 	       else 
-	             
-	 	   cout<<"Program sonlandý."; break;	// Bitirsin.
-	            
+
+ 	       else
+
+	 	   cout<<"Program sonlandï¿½."; break;	// Bitirsin.
+
         } // While sonu.
-       
+
     }
-    
+
     void para ()
-    {   
+    {
      double gelen,olasilik,i,sayi;
      double k,tut,devam;
     	while (1)
 		{
 		k=0;
-	
+
 		cout << "para kac kez atilsin:";
-    	cin >> i;  
+    	cin >> i;
     	tut = i;
     	don:
 		cout <<"hangi olasilik hesaplanacak yazi (0) yada tura(1) ??: ";
@@ -107,85 +107,79 @@ int c;
     	    if (gelen!=0 && gelen!=1 )
     	    {
     	       cout<< "yanlis deger girildi!"<< endl;
-    	       goto don;   
+    	       goto don;
 			}
 		srand(time (NULL));
-	    
+
 	        for (i; i > 0; i--)
 		    {
 	    	sayi = rand()%2;
-	    
+
 	    	if (sayi == 1)
 	    	    cout << "tura geldi"<<endl;
-	    	else 
+	    	else
 	    	    cout << "yazi geldi"<<endl;
 	    	if (sayi == gelen)
 			   k += 1;
-		    } 
-		    
+		    }
+
 			if (gelen == 1)
-				cout << "tura gelme olasiligi :"<< (k/tut)<< endl;	
+				cout << "tura gelme olasiligi :"<< (k/tut)<< endl;
 			if (gelen == 0)
 				cout << "yazi gelme olasiligi :"<< (k/tut)<< endl;
-				    
-		    
-		        cout<< "Deney tekrarý için: 1 - Deney sonu için: 2 ---> "; // Tekrar teklifi.
+
+
+		        cout<< "Deney tekrarï¿½ iï¿½in: 1 - Deney sonu iï¿½in: 2 ---> "; // Tekrar teklifi.
                 cin>> devam;
      	    if(devam == 1)
 	 	    {
 	 		cout<< endl; continue; // Devam etsin.
 	 	    }
- 	 	    else 
+ 	 	    else
 	 	    {
-	 		cout<< "Program sonlandý."; break;	// Bitirsin.
+	 		cout<< "Program sonlandï¿½."; break;	// Bitirsin.
 	 	    }
-     
+
        }
-	    
+
      }
-    
-    
+
+
 int main()
 { setlocale(LC_ALL,"Turkish");
 	int secim;
-	cout<<setw(5)<<"ÝÞLEMNO"<<"setw(20)"<<"YAPILACAK ÝÞLEM"<< endl
+	cout<<setw(5)<<"ï¿½ï¿½LEMNO"<<"setw(20)"<<"YAPILACAK ï¿½ï¿½LEM"<< endl
 		<<setw(3)<<"1"<<setw(35)<<"ORTALAMA-STANDART SAPMA HESABI"<<endl
 		<<setw(3)<<"2"<<setw(35)<<"OLASILIK HESABI - ZAR ATMA____"<<endl
 		<<setw(3)<<"3"<<setw(35)<<"OLASILIK HESABI - PARA ATMA___"<<endl
-		<<setw(3)<<"4"<<setw(35)<<"PROGRAMDAN ÇIKIÞ______________"<<endl<<endl;
+		<<setw(3)<<"4"<<setw(35)<<"PROGRAMDAN ï¿½IKIï¿½______________"<<endl<<endl;
 		tetik:
-		cout<<"Ýþlem seçiniz: ";
+		cout<<"ï¿½ï¿½lem seï¿½iniz: ";
 
 		cin>>secim;
 		switch(secim)
 		{
-			case 1: 
+			case 1:
 			{
-			hesap(); 
-			break;	
-			}
-			case 2: 
-			{
-			zar(); 
+			hesap();
 			break;
 			}
-			case 3: 
+			case 2:
 			{
-		     para(); 
+			zar();
 			break;
 			}
-			case 4: 
+			case 3:
 			{
-		    void uyu(); 
+		     para();
 			break;
 			}
-			default: cout<< "Geçersiz seçim." <<endl;
+			case 4:
+			{
+		    void uyu();
+			break;
+			}
+			default: cout<< "Geï¿½ersiz seï¿½im." <<endl;
 			goto tetik;
 		}
 }
-
-    
-    
-
-
-	 
