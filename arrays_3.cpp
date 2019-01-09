d6179dcad8cc7771c90ae5390b96510304dcd30a
Cagri Esen <esencgr@gmail.com>
2019-01-09 12:21:54 +0300

    delete file

--- arrays_3.cpp
+++ arrays_3.cpp
@@ -7,8 +7,8 @@ int main()
 int c[3][3];
 
   for (int i = 0; i < 3; i++)
-	    for (int j = 0; j < 3; j++)
-	         c[i][j] = i + j;
+      for (int j = 0; j < 3; j++)
+	   c[i][j] = i + j;
 
   for (int i = 0; i < 3; i++){
       for (int j = 0; j < 3; j++)
