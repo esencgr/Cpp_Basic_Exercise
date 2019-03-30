ceaedd5397cd8823bee69f4b3fea5c38aca336d4
Cagri Esen <esencgr@gmail.com>
2019-03-30 12:10:11 +0300

    modified

--- arrays_2.cpp
+++ arrays_2.cpp
@@ -5,31 +5,24 @@ using namespace std;
 int main ()
 {
    int a[5];
-
    for (int i = 0; i <= 4; i++)
    cin>> a[i];
 
    int eb = a[0];
-
        for (int i = 0; i <= 4; i++){
-
 	         if (a[i] > eb)
 	         eb = a[i];
        }
 
    cout<< "en buyuk sayi: " << eb << endl;
 
-
-    int ek = a[0];
-
+   int ek = a[0];
         for (int i = 0; i <= 4; i++){
-
 	         if (a[i] < ek)
 	         ek = a[i];
         }
 
    cout<< "en kucuk sayi: "<< ek <<endl;
-
 
     int sum = 0;
 
